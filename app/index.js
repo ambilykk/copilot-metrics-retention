d8ffb63b21d2ed7c3d1eeba28806a95b362bef80
ambilykk <ambilykk@github.com>
2024-04-17 14:37:09 +0900

    filtering and finding the delta

--- app/index.js
+++ app/index.js
@@ -60,6 +60,13 @@ async function run(org_Name, csv_path) {
                 if (jsonData.length > 0) {
                     let lastNode = jsonData[jsonData.length - 1]; // get the last node
                     console.log(lastNode);
+                    let lastDate = new Date(lastNode.day); // get the date of the last node
+                    console.log(lastDate);
+
+                    // remove the nodes from metricsData that are older than the last node in the JSON data array
+                    metricsData = metricsData.filter(node => new Date(node.day) > lastDate);
+                    console.log(JSON.stringify(metricsData));
+
                 } else {
                     console.log("The JSON data array is empty.");
                 }
